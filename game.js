edc073a4fb16c7a8409b18f8b2357f9de23f4f22
Im <sim35@gatech.edu>
2025-03-03 15:49:06 -0500

    drag void func update

--- game.js
+++ game.js
@@ -191,10 +191,13 @@ document.addEventListener("DOMContentLoaded", function () {
             player.dy = 0;
         });
     }
+    
+    document.addEventListener("touchmove", function (event) {
+        event.preventDefault();
+    }, { passive: false });
 
     // 🔥 키보드 입력 시 기본 스크롤 방지 (게임 실행 중일 때만)
     window.addEventListener("keydown", preventPageScroll);
-    //updateCanvas();
     startGame();
 
     /*exploreGameButton.addEventListener("click", function () {
